e25dbdadb018d51d6c1b16549f04bc225f70699e
Tony Bebber <tony.bebber@trackvia.com>
2013-11-03 01:16:43 -0600

    Updates to allow for goalie to be rendered as image

--- app/scripts/controllers/PlayerType.js
+++ app/scripts/controllers/PlayerType.js
@@ -2,27 +2,29 @@
 
 angular.module('playerCreatorApp').controller('PlayerTypeCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
 
-   	$scope.playerTypes = ['player', 'goalie', 'enforcer'];
-   	$scope.selectedPlayerType = null;
+ 	$scope.playerTypes = ['player', 'goalie', 'enforcer'];
+ 	$scope.selectedPlayerType = null;
 
-   	$scope.teamName = 'newyorkrangers';
-    $scope.jersey = 'home';
-    $scope.playerNumber = '99';
-    $scope.playerPosition = 'C';
+ 	$scope.teamName = 'newyorkrangers';
+  $scope.jersey = 'home';
+  $scope.playerNumber = '99';
+  $scope.playerPosition = 'C';
 
-    $scope.infoVisible = {};
 
-    $scope.showInfo = function(num) {
-    	$scope.infoVisible[num] = true;
-    };
+
+  $scope.infoVisible = {};
+
+  $scope.showInfo = function(num) {
+  	$scope.infoVisible[num] = true;
+  };
 
 	$scope.hideInfo = function(num) {
 		$scope.infoVisible[num] = false;
 	};
 
 	$scope.setPlayerType = function(type) {
-        $scope.player = type;
-        $routeParams.playerType = type;
+    $scope.player = type;
+    $routeParams.playerType = type;
 	};
 
 }]);
