9e22f0285b6a7587a5ae5ed42b5cfc1d78a74a0e
Tony Bebber <tony.bebber@trackvia.com>
2013-11-30 23:16:53 -0700

    Change reset button, also some css cleanup

--- app/scripts/directives/nav.js
+++ app/scripts/directives/nav.js
@@ -1,13 +1,25 @@
 'use strict';
 
-angular.module('playerCreatorApp').directive('nav', function () {
+angular.module('playerCreatorApp').directive('nav', ['$route', '$location', function ($route, $location) {
     return {
       	templateUrl: '/views/nav.html',
       	restrict: 'E',
       	link: function postLink(scope, element, attrs) {
 
-      		
+      		var checkIsHome = function() {
+      			if ($location.$$path === '/') {
+      				scope.isHome = true;
+      			} else {
+      				scope.isHome = false;
+      			}
+      		};
+
+      		checkIsHome();
+
+      		scope.$on('$locationChangeStart', function(event, next, current) { 
+				checkIsHome();      			
+			});
 
     	 }
     };
-});
+}]);
